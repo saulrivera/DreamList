d403479d6fc3d94c9b6f309173c9cc091457742e
Saúl Rivera <saulriveal@gmail.com>
2017-07-16 21:11:56 -0500

    TableView connected to View Controller

---
+++ DreamList/ItemCell.swift
@@ -0,0 +1,21 @@
+//
+//  ItemCell.swift
+//  DreamList
+//
+//  Created by Saul Rivera on 7/16/17.
+//  Copyright © 2017 Dymtech. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thum: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+
+    
+
+    
+}
