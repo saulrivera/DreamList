e4e6208f3932c1816721363f3ccc3237312fc51e
Saúl Rivera <saulriveal@gmail.com>
2017-07-16 21:00:44 -0500

    View designed, table view added with material design

--- DreamList/MainVC.swift
+++ DreamList/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  DreamList
 //
 //  Created by Saul Rivera on 7/16/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
